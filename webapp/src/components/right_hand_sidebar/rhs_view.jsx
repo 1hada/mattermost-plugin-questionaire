e33b926b8078258a6d49d5803892ff94b9c4d896
Jonathan Sanabria <45742788+1hada@users.noreply.github.com>
2025-06-13 13:02:23 -0400

    Require user set http:// and https:// for the address.

--- webapp/src/components/right_hand_sidebar/rhs_view.jsx
+++ webapp/src/components/right_hand_sidebar/rhs_view.jsx
@@ -27,7 +27,7 @@ export default class RHSView extends React.PureComponent {
     componentDidMount() {
         // Get the User settings
         this.loadSettings().then(() => {
-            fetch(`http://${this.state.questionServer}/buttons`)
+            fetch(`${this.state.questionServer}/buttons`)
                 .then((res) => res.json())
                 .then((data) => {
                     this.setState({
@@ -66,7 +66,7 @@ export default class RHSView extends React.PureComponent {
     }
 
     handleButtonClick = (buttonId) => {
-        fetch(`http://${this.state.questionServer}/button-click`, {
+        fetch(`${this.state.questionServer}/button-click`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
